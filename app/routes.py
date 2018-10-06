b531ff51279eac0eecfa26d3ac921280f20fbb85
Phillip Lange <langepj@gmail.com>
2018-10-06 17:01:32 +1300

    For loop in template

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "Phillip"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {
+            "author":{"username" : "Bob"},
+            "body" : "Wellington is beautiful today!"
+        },
+        {
+            "author":{"username" : "Jane"},
+            "body" : "It's not even summer and I have a tan line!"
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
